0a0f6d7cc3dd1bd218d9f17006ffb929b5e72cff
aleena30-05 <43938991+aleena30-05@users.noreply.github.com>
2023-09-30 17:34:41 +0500

    Add TopNav, Logo, basic cards

---
+++ App.js
@@ -0,0 +1,23 @@
+// const React = require('react');
+import * as React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
+import Header from './components/AppHeader';
+import NavBar from './components/NavBar';
+import MainScreen from './components/MainScreen';
+import LinearGradient from 'react-native-linear-gradient';
+
+export default function App() {
+  return (
+    <View style = {{flex: 1, justifyContent: "flex-start"}}> 
+    
+      <Header />
+      <NavBar />
+      <ScrollView>
+      <MainScreen />
+      </ScrollView>
+      
+      
+    </View>
+  );
+}
