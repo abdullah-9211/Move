e3f5ff89ad6f98d98226e8705d329bf77cbf028a
aleena30-05 <43938991+aleena30-05@users.noreply.github.com>
2023-10-16 21:48:17 +0500

    gradient experimentation

---
+++ Workouts.js
@@ -0,0 +1,22 @@
+// const React = require('react');
+import * as React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
+import Header from './components/AppHeader';
+import NavBar from './components/NavBar';
+import MainScreen from './components/MainScreen';
+import NavBarBot from './components/NavBarBot'
+import LinearGradient from 'react-native-linear-gradient';
+
+export default function Workouts() {
+  return (
+    <View style = {{flex: 1, justifyContent: "flex-start"}}> 
+      <Header />
+      <NavBar />
+      <ScrollView>
+      <MainScreen/>
+      </ScrollView>
+      <NavBarBot />
+    </View>
+  );
+}
