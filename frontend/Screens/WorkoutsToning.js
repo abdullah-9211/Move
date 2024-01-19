36986112947d979a433ac5c9a3b5faa9d3649eb9
Abdullah <95099545+abdullah-9211@users.noreply.github.com>
2024-01-19 21:40:15 +0500

    renders data from workoutsToning Screen

--- frontend/Screens/WorkoutsToning.js
+++ frontend/Screens/WorkoutsToning.js
@@ -1,45 +1,73 @@
 import * as React from 'react';
-import { Image, FlatList, horizontal, View, StyleSheet, Text, Dimensions, Pressable } from 'react-native';
+import { Image, FlatList, horizontal, View, StyleSheet, Text, Dimensions, Pressable, Modal, ActivityIndicator } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 import { useFonts } from 'expo-font';
 import NavBarBot from '../components/NavBarBot';
 import NavBar from '../components/NavBar';
 import { useNavigation, useRoute } from '@react-navigation/native';
+import axios from 'axios';
+import {API_URL} from "@env"
 
 const { width: screenWidth } = Dimensions.get('window');
 
-const ListItem = ({ item }) => {
-
-  const navigation = useNavigation();
-
-  const route = useRoute();
-  const user = route.params?.user;
-  const workouts = route.params?.workouts;
-
-  
-  return (
-    <LinearGradient
-      colors={['rgba(0,0,0,0)', 'rgba(0,0,0,0.2)']}
-      style={styles.gradient}
-    >
-    <Pressable onPress={() => navigation.navigate('StartWorkout', {user: user})}>
-      <Image
-        source={{
-          uri: item.plan_image,
-        }}
-        style={{ width: screenWidth / 2 - 40, height: 135, borderRadius: 9 }}
-        resizeMode="cover"
-      />
-      </Pressable>
-    </LinearGradient>
-  );
-};
 
 export default function WorkoutsToning() {
+
+  const ListItem = ({ item }) => {
+
+    const navigation = useNavigation();
+  
+    const route = useRoute();
+    const user = route.params?.user;
+    const workouts = route.params?.workouts;
+  
+    const handleWorkoutClick = (item) => () => {
+      setLoading(true);
+  
+      const apiUrl = API_URL + '/exercise/get-exercises/' + item.id;
+      axios.get(apiUrl)
+      .then((response) => {
+        console.log(response.data);
+        const exercises_data = response.data;
+        const apiUrl = API_URL + '/exercise/get-plan-trainer/' + item.plan_trainer;
+        axios.get(apiUrl)
+        .then((response) => {
+          console.log(response.data);
+          setLoading(false);
+          navigation.navigate('StartWorkout', {user: user, workout: item, exercises: exercises_data, trainer: response.data[0]});
+        }
+        )
+      })
+      .catch((error) => {
+        console.log(error);
+      })    
+  
+    }
+    
+    return (
+      <LinearGradient
+        colors={['rgba(0,0,0,0)', 'rgba(0,0,0,0.2)']}
+        style={styles.gradient}
+      >
+      <Pressable onPress={handleWorkoutClick(item)}>
+        <Image
+          source={{
+            uri: item.plan_image,
+          }}
+          style={{ width: screenWidth / 2 - 40, height: 135, borderRadius: 9 }}
+          resizeMode="cover"
+        />
+        </Pressable>
+      </LinearGradient>
+    );
+  };
+  
+
   const navigation = useNavigation();
   const route = useRoute();
   const user = route.params?.user;
   const workouts = route.params?.workouts;
+  const [loading, setLoading] = React.useState(false);
 
   const [loaded] = useFonts({
     'QuickSand': require('../assets/fonts/Quicksand-SemiBold.ttf'),
@@ -60,7 +88,13 @@ export default function WorkoutsToning() {
         <Image source={require('../assets/images/sort.png')}
             style={{ width: 20, height: 20 }} />
       </View>
-
+      {loading && (
+                <Modal transparent={true} animationType="fade">
+                <View style={styles.modal}>
+                    <ActivityIndicator size="large" color="#fff" />
+                </View>
+                </Modal>
+      )}
       <FlatList
         contentContainerStyle={{ paddingHorizontal: 10 }}
         data={workouts}
@@ -105,6 +139,12 @@ const styles = StyleSheet.create({
     marginHorizontal: 20,
     marginVertical: 20,
     borderRadius: 12,
+  },
+  modal: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)', // Semi-transparent background
   },
 });
 
