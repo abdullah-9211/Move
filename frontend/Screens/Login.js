fee379cafafb6258b2f53bc1c1618e3f50f3cef0
aleena30-05 <43938991+aleena30-05@users.noreply.github.com>
2024-01-24 22:37:20 +0500

    fixed not working

--- frontend/Screens/Login.js
+++ frontend/Screens/Login.js
@@ -31,6 +31,7 @@ export default function Login() {
         setLoading(true);
         try {
             const apiUrl = API_URL + '/user/login';
+            console.log("test : " + apiUrl);
             const requestBody = {
                 email: email,
                 password: password
@@ -52,9 +53,26 @@ export default function Login() {
                 }
             }
 
-        } catch (error) {
-            console.log(error);
+        } 
+        catch (error) {
+            if (error.response) {
+                // The request was made and the server responded with a status code
+                // that falls out of the range of 2xx
+                console.log(error.response.data);
+                console.log(error.response.status);
+                console.log(error.response.headers);
+            } else if (error.request) {
+                // The request was made but no response was received
+                console.log(error.request);
+            } else {
+                // Something happened in setting up the request that triggered an Error
+                console.log('Error', error.message);
+            }
         }
+        
+        // catch (error) {
+        //     console.log(error);
+        // }
         setLoading(false);
     }
 
