12efacf60aba4c0b3a107e31d4c0e8936b1dea0f
[Aleena Adil] <leena051100@gmail.com>
2024-03-28 01:14:14 +0500

    commit

--- frontend/Screens/TrainerScreen.js
+++ frontend/Screens/TrainerScreen.js
@@ -122,15 +122,7 @@ const ListItem = ({ item }) => {
   return (
     <View style={styles.container}>
       <NavBar />
-      <View style={{marginTop: 120, flexDirection:"row",justifyContent: "space-between", alignItems: "center", marginRight:25, marginBottom:10}}>
-        <View style={{ flexDirection: "row", alignItems: "center" }}>
-        <Text style={styles.headingtext}>FILTER</Text>
-        <Image source={require('../assets/images/filter.png')} 
-          style={{ marginLeft: 10, width: 20, height: 20 }} />
-      </View>
-        <Image source={require('../assets/images/sort.png')}
-            style={{ width: 20, height: 20 }} />
-      </View>
+      <View style={{marginTop:100}}>
 
       <FlatList
         contentContainerStyle={{ paddingHorizontal: 10 }}
@@ -140,6 +132,7 @@ const ListItem = ({ item }) => {
         numColumns={2} // Set the number of columns to 2
         showsHorizontalScrollIndicator={false}
       />
+      </View>
 
       <View style={{ marginBottom: 65 }} />
 
